b159319c71e538f0612abd03b2404cbf7399125b
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-01-10 17:12:55 +0500

    07-01-2023

---
+++ 07-01-2023/app.js
@@ -0,0 +1,75 @@
+// let number1=+prompt("Enter first number")
+// var number2=+prompt("Enter second number")
+// var sum=number1+number2
+// var difference=number1-number2
+// var product=number1*number2
+// var quotient=number1/number2
+// if(number1===8){
+//     alert("Number 1 is 8")
+// }
+// else if(number2===8){
+//     alert("Number 2 is 8")
+// }
+// else if(sum===8){
+//     alert("Sum is 8")
+// }
+// else if(difference===8){
+//     alert("Difference is 8")
+// }
+// else if(product===8){
+//     alert("Product is 8")
+// }
+// else if(quotient===8){
+//     alert("Quotient is 8")
+// }
+// else{
+//     alert("No 8 is found")
+// }
+
+// let alpha=prompt("Enter an alphabet")
+// if(alpha==="a"||alpha==="A"){
+//     alert("it is vowel")
+// }
+// else if(alpha==="e"){
+//     alert("it is vowel")
+// }
+// else{
+//     alert("It is consonant")
+// }
+
+// var num=+prompt("Enter a number")
+// if(num>=40&&num<=60){
+//     alert("yes")
+// }
+// else{
+//     alert("no")
+// }
+// var num=+prompt("Enter a number")
+
+// if(num%2===0){
+//     alert("It is even")
+// }
+
+// else if(num%2!==0){
+//     alert("It is odd")
+// }
+// else{
+//     alert("Invalid Input")
+// }
+
+var alpha=prompt("Enter an alphabet")
+
+switch(alpha){
+    case "a":
+        alert("It is vowel")
+        break
+        
+    case "e":
+        alert("It is vowel")
+        break
+    default:
+        alert("It is a consonant")
+        break
+}
+
+
